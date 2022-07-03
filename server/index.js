28310ba49e5c3c811fa751705a2ec4d47c87576d
rnhkz <alb190010@utdallas.edu>
2022-07-03 15:58:21 -0500

    Initial commit: Server implementation

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const {graphqlHTTP} = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+app.routes(cors());
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === 'development',
+    })
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
